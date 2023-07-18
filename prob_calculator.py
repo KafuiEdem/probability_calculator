0a405309ff7851bea497f392303b7cc7a38dcc78
reamedzo@st.ug.edu.gh <reamedzo@st.ug.edu.gh>
2023-07-18 03:20:21 +0000

    Final update

--- prob_calculator.py
+++ prob_calculator.py
@@ -1,48 +1,44 @@
 import copy
 import random
 
-# Consider using the modules imported above.
-
 class Hat:
-    def __init__(self,**kw) -> None:
-        self.balls=kw
+    def __init__(self, **balls) -> None:
+        self.balls = balls
         self.contents = []
 
-        for list in self.balls:
-            self.contents.append(list)
+        for ball, count in self.balls.items():
+            for _ in range(count):
+                self.contents.append(ball)
 
-    def draw(self,num_ball):
-        lup = []
-        if num_ball < len(self.contents):
+    def draw(self, num_ball):
+        if num_ball >= len(self.contents):
             return self.contents
-        else:
-            for _ in range(0,num_ball):
-                c = random.choice(self.contents)
-                lup.append(c)
-                p = set(self.contents) ^ set(lup)
-            return lup
-       
-        # return random.choice(self.contents)
 
+        drawn_balls = random.sample(self.contents, num_ball)
+        for ball in drawn_balls:
+            self.contents.remove(ball)
+
+        return drawn_balls
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    c = []
-    count = 0
-    ball = hat.draw(num_balls_drawn)
-    for b in expected_balls:
-        c.append(b)
-    
-    for i in ball:
-        for u in c:
-            if i == u:
-                count +=1
-                file = f"{u}:{count}"
-    return file
+    experiments_passed = 0
 
+    for _ in range(num_experiments):
+        hat_copy = copy.deepcopy(hat)
+        drawn_balls = hat_copy.draw(num_balls_drawn)
+
+        passed = True
+        for ball, count in expected_balls.items():
+            if drawn_balls.count(ball) < count:
+                passed = False
+                break
+
+        if passed:
+            experiments_passed += 1
+
+    probability = experiments_passed / num_experiments
+    return probability
 
 hat = Hat(black=6, red=4, green=3)
-probability = experiment(hat=hat,
-                  expected_balls={"red":2,"green":1},
-                  num_balls_drawn=5,
-                  num_experiments=2000)
+probability = experiment(hat=hat, expected_balls={"red": 2, "green": 1}, num_balls_drawn=5, num_experiments=2000)
 print(probability)
